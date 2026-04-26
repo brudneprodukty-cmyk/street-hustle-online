8e9d6ca34365e1562a8f46058ee2bd42721d0005
brudneprodukty-cmyk <brudneprodukty@gmail.com>
2026-04-26 14:35:08 +0100

    server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-/* ===== SERVE YOUR GAME ===== */
+/* ===== SERVE GAME ===== */
 app.use(express.static(path.join(__dirname, "public")));
 
 /* ===== PLAYER STORAGE ===== */
@@ -30,39 +30,42 @@ io.on("connection", (socket) => {
   /* SEND ALL PLAYERS TO NEW PLAYER */
   socket.emit("currentPlayers", players);
 
-  /* TELL OTHERS ABOUT NEW PLAYER */
+  /* TELL OTHERS */
   socket.broadcast.emit("newPlayer", {
     id: socket.id,
     ...players[socket.id]
   });
 
-  /* ===== MOVEMENT + STATE ===== */
+  /* ===== MOVEMENT SYNC ===== */
   socket.on("move", (data) => {
-    if(players[socket.id]){
-      players[socket.id] = {
-        ...players[socket.id],
-        x: data.x,
-        y: data.y,
-        dir: data.dir,
-        moving: data.moving,
-        frame: data.frame
-      };
-    }
+
+    if(!players[socket.id]) return;
+
+    players[socket.id] = {
+      x: data.x,
+      y: data.y,
+      dir: data.dir || "down",
+      moving: data.moving || false,
+      frame: data.frame || 0
+    };
+
   });
 
   /* ===== DISCONNECT ===== */
   socket.on("disconnect", () => {
     console.log("Player left:", socket.id);
+
     delete players[socket.id];
+
     io.emit("playerDisconnected", socket.id);
   });
 
 });
 
-/* ===== GAME LOOP (SYNC) ===== */
+/* ===== GAME LOOP (SYNC ALL PLAYERS) ===== */
 setInterval(() => {
   io.emit("updatePlayers", players);
-}, 1000 / 30);
+}, 1000 / 30); // 30 FPS
 
 /* ===== START SERVER ===== */
 const PORT = process.env.PORT || 3000;
